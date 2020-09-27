1cece22008364725f88dfb12c40bf7ede4c3f262
DoyonKim <bluesky13a@gmail.com>
2020-09-27 17:31:28 +0900

    change handlebars to react.

---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import * as React from "react"
+import * as ReactDOM from "react-dom"
+import Counter from "./component/Counter"
+
+ReactDOM.render(<Counter name="React" />, document.getElementById("app"))
