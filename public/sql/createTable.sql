eaf25aeafcb323469d0f27a7f0e68c6e0f3b2d17
DoyonKim <bluesky13a@gmail.com>
2020-11-11 15:27:26 +0900

    change folder name

---
+++ public/sql/createTable.sql
@@ -0,0 +1,10 @@
+    CREATE TABLE CARD (
+        ID INT(7) NOT NUYLL auto_increament,
+        CONTENTS VARCHAR(200) NOT NULL,
+        DAY DATE NOT NULL,
+
+    );
+     create table test (
+    -> id int(10) not null auto_increament,
+    -> contents varchar(200))
+    -> ;
